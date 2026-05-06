ec77478d4f75365937dcec97fb0fcd2b76f0c8ce
riccardo <colaninnoriccardo@gmail.com>
2026-05-06 12:46:55 +0200

    Correzione del database

--- classi/credenziali.php
+++ classi/credenziali.php
@@ -1,46 +1,50 @@
 <?php 
+require_once __DIR__."/database.php";
 
 class Credenziali {
+    private $db;
     
-
     public function __construct() {
-        
+        $this->db = new database();
     }
 
-    public function doLogin($user,$password) {
+    public function doLogin($user, $password) {
+        $conn = $this->db->getConnection();
         
-        require_once __DIR__."/database.php";
-        $db = new database();
-        $r = $db->query("SELECT * FROM user WHERE username='$user' AND password='$password'");
-        if ($row = $r->fetch_assoc()) {
+        $stmt = $conn->prepare("SELECT * FROM `USER` WHERE Username = ? AND Password = ?");
+        $stmt->bind_param("ss", $user, $password); 
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()) {
             return true;
         }
         return false;
     }
     
-    public function doRegister($user,$email,$password) {
-        require_once __DIR__."/database.php";
-        $db = new database();
+    public function doRegister($user, $email, $password) {
+        $conn = $this->db->getConnection();
         $otp = $this->codiceOTP();
-        $r = $db->query("INSERT INTO user (username,email,password,CodiceOTP) VALUES ('$user','$email','$password','$otp')");
-        if ($r) {
-            return true;
-        }       
-         return false;
-    }
+        
+        $stmt = $conn->prepare("INSERT INTO `USER` (Username, Email, Password, CodiceOTP) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $user, $email, $password, $otp);
 
+        try {
+            if ($stmt->execute()) {
+                return true;
+            }
+        } catch (mysqli_sql_exception $e) {
+            return false;
+        }
+        return false;
+    }
     
     public function codiceOTP() {
         $data = random_bytes(16);
-
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // Version 4
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // Variant 10
-
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
-
-    
 }
-
-
 ?>
--- classi/database.php
+++ classi/database.php
@@ -1,22 +1,26 @@
 <?php
-
+require_once __DIR__ . '/../config/config.php';
 
 class Database {
     private $conn;
 
     public function __construct() {
-        require_once __DIR__ . '/../config/config.php';
-        $this->conn = new mysqli(Config::$hostname, Config::$username, Config::$password, Config::$dbName);
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         
+        try {
+            $this->conn = new mysqli(Config::$hostname, Config::$username, Config::$password, Config::$dbName);
+            $this->conn->set_charset("utf8mb4");
+        } catch (mysqli_sql_exception $e) {
+            die("ERRORE FATALE - Connessione al database fallita: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
     }
 
     public function query($q) {
         return $this->conn->query($q);
     }
-
 }
-
 ?>
